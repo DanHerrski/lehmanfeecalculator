8d44c92f3ac28b2a457c7e69b7269ffa4c0535b0
DanHerrski <Daniel.Herr@gmail.com>
2015-12-21 17:05:28 -0800

    fix from prompt to alert

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var isChrome = !!window.chrome && !isOpera;              // Chrome 1+
 var isIE = /*@cc_on!@*/false || !!document.documentMode;   // At least IE6
 
 if (isFirefox){
-  var sign = window.prompt('Apologies, we have been having issues with Firefox and this Script. Please use IE or Chrome for the time being.','Okay')  
+  window.alert('Apologies, we have been having issues with Firefox and this Script. Please use IE or Chrome for the time being.')  
 };
 
 
